146fa522f953fc5adacf37db5e306c6e5203ff4f
Faris <fajeddig@hotmail.fr>
2019-02-06 21:59:36 +0100

    Style and add button Reload

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,33 +1,49 @@
 // Home.js
 
 import React, { Component } from 'react';
-import { Alert, View, Text, Button, FlatList } from 'react-native';
+import { Alert, View, Text, Button, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+
 
 export class Home extends Component {
 
+  getInitialState = () => {
+     const initialState = {
+       numberOfLife: 5,
+       question: "Question 1",
+       answers: [
+         {key: 'a', value: 'Réponse A (bonne)', answer: true},
+         {key: 'b', value: 'Réponse B', answer: false},
+         {key: "c", value: "Réponse C", answer: false},
+         {key: 'd', value: 'Réponse D', answer: false}
+       ]
+     };
+     return initialState;
+  }
+
   constructor (props) {
     super(props)
-    this.state={
-      numberOfLife: 5,
-      question: "Question 1",
-      answers: [{key: 'a', value: 'Réponse A (bonne)', answer: true}, {key: 'b', value: 'Réponse B', answer: false}, {key: "c", value: "Réponse C", answer: false}, {key: 'd', value: 'Réponse D', answer: false}]
-    }
+    this.state=this.getInitialState()
+  }
+
+  _getBackToInitialState(){
+    this.setState(this.getInitialState());
   }
 
   render() {
     console.log("test");
     return (
       <View>
-        <Text>QCM Expressions quebecoises</Text>
-        <Text> Nombre de vie : {this.state.numberOfLife} </Text>
-        {/* <Button onPress={() => this.props.navigation.navigate('Settings')} title="Settings"/> */}
-        <View>
-          <Text>{this.state.question}</Text>
+        <View style={{flexDirection: 'row', marginTop: 20}}>
+          {/* <Button onPress={() => this.props.navigation.navigate('Settings')} title="Settings"/> */}
+          <View style={styles.question}>
+            <Text style={{fontSize: 26, textAlign: 'center'}}>{this.state.question}</Text>
+          </View>
+          <Text style={styles.numberLife}>{this.state.numberOfLife} vies restantes ! </Text>
         </View>
         <FlatList
           data={this.state.answers}
           renderItem={({item}) =>
-            <View>
+            <View style={styles.answer}>
               <Button
                 onPress={() => {
                   if (item.answer){
@@ -47,9 +63,29 @@ export class Home extends Component {
             </View>
             }
         />
+        <TouchableOpacity onPress={() => this._getBackToInitialState()}>
+          <Text>Recommencer </Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  numberLife: {
+    flex: 1,
+    color: 'rgb(195, 18, 18)'
+  },
+  question: {
+    flex: 2,
+    color: 'rgb(233, 166, 86)',
+    marginBottom: 20
+  },
+  answer: {
+    flex: 1,
+    marginTop: 10,
+    marginBottom: 10
+  }
+})
 
 export default Home
